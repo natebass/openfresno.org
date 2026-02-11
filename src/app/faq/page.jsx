081ff59197de9ce657130a7d723b7cba89efdc10
Nate Bass <nw3455@gmail.com>
2026-02-10 17:15:54 -0800

    Refactor the FAQ page.

--- src/app/faq/page.jsx
+++ src/app/faq/page.jsx
@@ -1,10 +1,77 @@
-import Faq from "./faq";
 import Layout from "@/components/layout/layout";
+import MDXComponents from "./MDXComponents";
+import {SectionType} from "@/utility/constants/theme";
+import fs from "fs";
+import {MDXRemote} from "next-mdx-remote/rsc";
+import path from "path";
+import Link from "next/link";
+import GeneralSection from "@/components/ui/GeneralSection";
 
+function extractToc(content) {
+    const headers = [];
+    const headerRegex = /^###\s+(.+)$/gm;
+    let match;
+    while ((match = headerRegex.exec(content)) !== null) {
+        const text = match[1];
+        const slug = text.toLowerCase().replace(/\s/g, "_");
+        headers.push({text, slug});
+    }
+    return headers;
+}
+
+const rootDir = path.join(process.cwd(), "src", "app", "faq");
+
+function fetchMdx() {
+    const filePath = path.join(rootDir, "faqMDXTexts.mdx");
+    const fileContent = fs.readFileSync(filePath, {encoding: "utf-8"});
+    const tableOfContents = extractToc(fileContent);
+    return {fileContent, tableOfContents};
+}
+
+/**
+ * Frequently-asked questions page.
+ */
 export default function FaqPage() {
-  return (
-    <Layout>
-      <Faq />
-    </Layout>
-  );
+    const {fileContent, tableOfContents} = fetchMdx();
+    return (
+        <Layout>
+            <div className="faq-main">
+                <GeneralSection
+                    heading="FAQ"
+                    subHeading="Frequently Asked Questions"
+                    sectionType={SectionType.light}
+                >
+                    Welcome to our FAQ page, designed to provide you with quick and
+                    informative answers to commonly asked questions about Open Fresno. Whether
+                    you&apos;re curious about volunteering, projects, collaborations, or more,
+                    we&apos;ve compiled a list of queries to guide you through our mission,
+                    initiatives, and how you can get involved. Browse through the questions to
+                    find the information you need and embark on a journey of civic engagement,
+                    innovation, and community empowerment.
+                </GeneralSection>
+                <section
+                    className={`faq-container app-color--${SectionType.light} py-6 px-6`}
+                >
+                    <nav
+                        className={`mx-auto flex max-w-[calc(var(--screen-xxl)+80px)] flex-col max-lg:gap-4 lg:flex-row lg:justify-between`}
+                    >
+                        {tableOfContents.map((item) => (
+                            <Link
+                                key={item.slug}
+                                className="nav-link underline"
+                                href={`#${item.slug}`}
+                            >
+                                {item.text}
+                            </Link>
+                        ))}
+                    </nav>
+                </section>
+                <section
+                    className="faq-container-margins mx-auto flex max-w-[calc(var(--screen-xxl)+80px)] flex-col gap-0 px-6 py-t lg:pt-12 lg:pb-17">
+                    <MDXRemote source={fileContent} components={MDXComponents}/>
+                </section>
+                <hr className="mx-auto h-px max-w-(--screen-xxl) border-0 bg-(--neutral-400)"/>
+            </div>
+        </Layout>
+    );
 }
